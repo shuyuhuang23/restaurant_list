889d79f91dd5dd399a15f1882345644bd587390b
shuyuhuang23 <susan50337@gmail.com>
2022-01-23 11:23:28 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,10 +5,18 @@ const User = require('../../models/user')
 router.get('/login', (req, res) => {
     res.render('login')
 })
-// router.post('/login')
 
-// router.get('/register')
-// router.post('/register')
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.post('/register', (req, res) => {
+
+})
 // router.get('/logout')
 
 module.exports = router
