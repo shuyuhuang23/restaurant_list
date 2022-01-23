db0634915994c5770324f71a0bd5902b072947b1
shuyuhuang23 <susan50337@gmail.com>
2022-01-23 14:00:15 +0800

    feat: add user logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -37,6 +37,9 @@ router.post('/register', (req, res) => {
             }
         })
 })
-// router.get('/logout')
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/users/login')
+})
 
 module.exports = router
